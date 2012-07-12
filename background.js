77739df438df4f69dfe91f1a82d6f69b71a2b6dc
George Shank <shankga@gmail.com>
2012-07-11 18:18:59 -0600

    Better logic for walls and direction

--- background.js
+++ background.js
@@ -1,6 +1,8 @@
+/*global chrome:true */
+"use strict";
 chrome.experimental.app.onLaunched.addListener(function() {
   chrome.appWindow.create('window.html', {
-    'width': 500,
-    'height': 500
+    'width': 520,
+    'height': 520
   });
 });
--- snake.js
+++ snake.js
@@ -1,49 +1,80 @@
+"use strict";
 var arrows = {
   37: "LEFT",
   39: "RIGHT",
   38: "UP"   ,
-  40: "DOWN" 
+  40: "DOWN"
 };
+var antiArrows = {
+  "LEFT": "RIGHT",
+  "RIGHT": "LEFT",
+  "UP": "DOWN",
+  "DOWN": "UP"
+};
+
+var moved = false;
 
 var pos = {
   x: 0,
   y: 0
 };
 
-var context;
+var context
+  , direction = "RIGHT"
+  , canvas = document.getElementById('board');
 
-var canvas = document.getElementById('board');
+window.onload = function(){
+  if(canvas.getContext){
+    context = canvas.getContext('2d');
+    drawSnake(context, pos);
+  }
 
-if(canvas.getContext){
-  context = canvas.getContext('2d');
-  drawSnake(context, pos);
-}
+  setInterval(function(){
+    move(direction);
+  }, 500);
+};
 
-document.onkeydown = function(e){
-  if(arrows[e.keyCode] === "RIGHT"){
-    pos.x+=5;
+function move(direction){
+  switch(direction){
+    case "LEFT":
+      pos.x-= 5;
+      break;
+    case "RIGHT":
+      pos.x+= 5;
+      break;
+    case "UP":
+      pos.y-= 5;
+      break;
+    case "DOWN":
+      pos.y+= 5;
+      break;
   }
-  else if(arrows[e.keyCode] === "LEFT"){
-    pos.x-=5;
-  }
-  else if(arrows[e.keyCode] === "UP"){
-    pos.y-=5;
-  }
-  else if(arrows[e.keyCode] === "DOWN"){
-    pos.y+=5;
-  }
-  for(p in pos){
+  //Check to make sure we're not at the wall
+  for(var p in pos){
     if(pos[p] < 0)
-      pos[p] = 0;
+      die();
     else if(pos[p]>485){
-      pos[p] = 485;
+      die();
     }
   }
   drawSnake(context, pos);
 }
 
+function die(){
+  pos.x = 0;
+  pos.y = 0;
+  direction = "RIGHT";
+}
+
 function drawSnake(context, pos){
   canvas.width = canvas.width; //clears canvas
-
+  moved = true;
   context.fillRect(pos.x, pos.y, 15, 15);
 }
+
+document.onkeydown = function(e){
+  if(moved && arrows[e.keyCode] && direction !== antiArrows[arrows[e.keyCode]]){
+    direction = arrows[e.keyCode];
+    moved = false;
+  }
+};
